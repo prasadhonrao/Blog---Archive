089a7ebf41f537613256e3d0394c23d419b4b4e6
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-18 23:02:53 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 16/Code/AngularJSDataBinding/js/default.js
@@ -0,0 +1,16 @@
+﻿function EmployeeController($scope) {
+    $scope.Employees = [
+        { name: 'Scott Allen', company: 'OdeToCode' },
+        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+        { name: 'Scott Hanselman', company: 'Microsoft' },
+        { name: 'John Papa', company: 'Pluralsight' },
+    ];
+
+    $scope.addEmployee = function () {
+        $scope.Employees.push({
+            name: $scope.newEmployee.name,
+            company: $scope.newEmployee.company
+        });
+    }
+}
+
