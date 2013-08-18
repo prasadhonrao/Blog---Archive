a8a94d63e6ceefe7568fbb6582ea3d6599a7a553
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-18 23:31:37 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 12/Code/Data Binding/js/default.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    var app = WinJS.Application;
+    app.onactivated = function (args) {
+        // Custom model with 'name' attribute
+        var customer = WinJS.Binding.as({
+            name: ''
+        });
+
+        //// Bind the model attribute name to Output span
+        //var outputSpan = document.getElementById('OutputSpan');
+        //customer.bind('name', function (value) {
+        //    outputSpan.textContent = value;
+        //});
+
+        // Update model value [name] when user types in input textbox
+        var inputText = document.getElementById('InputText');
+        inputText.onkeyup = function () {
+            customer.name = inputText.value;
+        }
+
+        WinJS.UI.processAll();
+        WinJS.Binding.processAll(document.rootElement, customer);
+        
+    };
+
+app.onactivated = function (args) {
+    // Custom model with 'name' attribute
+    var customer = WinJS.Binding.as({
+        name: ''
+    });
+
+    // Update model value [name] when user types in input textbox
+    var inputText = document.getElementById('InputText');
+    inputText.onkeyup = function () {
+        customer.name = inputText.value;
+    }
+
+    WinJS.UI.processAll();
+    WinJS.Binding.processAll(document.rootElement, customer);
+};
+
+    app.start();
+})();
