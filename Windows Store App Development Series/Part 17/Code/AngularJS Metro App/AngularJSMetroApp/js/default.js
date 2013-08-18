b875d124471126623d309805d1b6cdcbbabce70f
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-18 22:53:48 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 17/Code/AngularJS Metro App/AngularJSMetroApp/js/default.js
@@ -0,0 +1,32 @@
+﻿function EmployeeController($scope) {
+
+    var app = WinJS.Application;
+    var dataList;
+    var employeesList;
+
+    employees = [
+        { name: 'Scott Allen', company: 'OdeToCode' },
+        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+        { name: 'Scott Hanselman', company: 'Microsoft' },
+        { name: 'John Papa', company: 'Pluralsight' },
+    ];
+
+    app.onactivated = function (args) {
+        WinJS.UI.processAll();
+    };
+
+    app.onready = function () {
+        dataList = new WinJS.Binding.List(employees);
+        employeesList = document.getElementById('employeesListView').winControl;
+        employeesList.itemDataSource = dataList.dataSource;
+    }
+
+    $scope.addEmployee = function () {
+        dataList.push(
+            { name: 'Prasad Honrao', company: 'Cognizant' },
+            { name: 'Scott Guthrie', company: 'Microsoft' }
+            );
+    }
+
+    app.start();
+}
