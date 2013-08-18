85e534b720c66373a8e417e240136f0a8d365858
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-18 23:39:24 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 11/Code/Class And Namespace/ClassAndNamespace/js/child.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    var Person = WinJS.Class.define(function (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    },
+    {
+        sayHello: function () {
+            console.log('Hello there good sir');
+        }
+    });
+
+    var Employee = WinJS.Class.derive(Person, function (firstName, lastName, position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    });
+
+})();
---
+++ Windows Store App Development Series/Part 11/Code/Class And Namespace/ClassAndNamespace/js/default.js
@@ -0,0 +1,66 @@
+﻿(function () {
+
+    WinJS.Namespace.define('MyFactory', {
+        id: function (id) {
+            return document.getElementById(id);
+        },
+
+        Car: WinJS.Class.define(
+            // Constructor
+            function (color, make) {
+                this.color = color;
+                this.make = make;
+            },
+            // Instance Members
+            {
+                Accelerate: function () {
+                    // function definition goes here
+                },
+
+                Turn: function () {
+                    // function definition goes here
+                }
+            },
+            // Static Members
+            {
+                numberOfWheels: 4
+            }
+        ),
+
+        Person: WinJS.Class.define(function (firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        },
+           {
+               sayHello: function () {
+                   console.log('Hello there good sir');
+               }
+           }),
+
+        Employee: WinJS.Class.derive(Person, function (firstName, lastName, position) {
+            
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.position = position;
+        }),
+
+        //AutomaticCar : WinJS.Class.derive(Car),
+
+    });
+
+
+    var app = WinJS.Application;
+    app.onactivated = function (args) {
+        var myCar = new MyFactory.Car('White', 'Honda');
+        var outputDiv = MyFactory.id('OutputDiv');
+
+        var emp = new MyFactory.Employee(1, 2, 3);
+        emp.sayHello();
+        outputDiv.textContent = 'Your car is ' + myCar.make;
+
+        //new MyFactory.MyClass(outputDiv);
+        WinJS.UI.processAll();
+    };
+
+    app.start();
+})();
