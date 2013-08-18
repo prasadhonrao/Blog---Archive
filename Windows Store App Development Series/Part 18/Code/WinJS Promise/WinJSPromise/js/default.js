76db2746b3db8765fc0946eb6b353e3c5c36f06e
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-18 22:18:56 +0100

    Data populated using XHR

--- Windows Store App Development Series/Part 18/Code/WinJS Promise/WinJSPromise/js/default.js
+++ Windows Store App Development Series/Part 18/Code/WinJS Promise/WinJSPromise/js/default.js
@@ -1,34 +1,61 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
-(function () {
+﻿(function () {
     "use strict";
 
     WinJS.Binding.optimizeBindingReferences = true;
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var articlesList = [];
+    var dataList = [];
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
-            args.setPromise(WinJS.UI.processAll());
+
+            var requestUrl = "http://prasadhonrao.com/feed/";
+
+            WinJS.xhr({ url: requestUrl})
+                 .done(
+                    // Complete function
+                    function (response) {
+                        var items = response.responseXML.querySelectorAll("item");
+
+                        for (var n = 0; n < items.length; n++) {
+                            var article = {};
+                            article.title = items[n].querySelector("title").textContent;
+                            article.pubDate = items[n].querySelector("pubDate").textContent;
+                            articlesList.push(article);
+                            //var thumbs = items[n].querySelectorAll("thumbnail");
+                            //if (thumbs.length > 1) {
+                            //    article.thumbnail = thumbs[1].attributes.getNamedItem("url").textContent;
+                            //    article.content = items[n].textContent;
+                            //    articlesList.push(article);
+                            //}
+                        }
+
+                        dataList = new WinJS.Binding.List(articlesList);
+                        var articleListView = document.getElementById('articleListView').winControl;
+                        articleListView.itemDataSource = dataList.dataSource;
+                    },
+
+                    // Error function
+                    function (request) {
+                        var x = 0;
+                    },
+
+                    // Progress function
+                    function (request) {
+                        var x = 0;
+                    }
+                );
+
+            WinJS.UI.processAll();
         }
     };
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+
+    app.onerror = function (args) {
+        var e = 0;
+    }
 
     app.start();
 })();
