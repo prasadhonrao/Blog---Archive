4a8e8a1e8d64774a9af116b24f07ee5527c76bc9
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-19 23:51:11 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 07/Code/AppBarCommand/AppBarCommand/js/default.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+            } else {
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.onready =  function (args) {
+        var containerDiv = document.getElementById('containerDiv');
+        var displayCommand = document.getElementById('DisplayCommand').addEventListener('click', DisplayMessage);
+        var hideommand = document.getElementById('HideCommand').addEventListener('click', HideMessage);
+    }
+
+    function DisplayMessage() {
+        containerDiv.innerHTML = "<h1>Windows 8 Rocks!</h1>"
+    }
+
+    function HideMessage() {
+        containerDiv.innerHTML = ""
+    }
+
+    app.oncheckpoint = function (args) {
+    };
+
+    app.start();
+})();
