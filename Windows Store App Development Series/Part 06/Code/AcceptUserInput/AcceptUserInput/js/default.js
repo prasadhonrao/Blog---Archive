7b02622db9e915e0f76f3588dedc5a71313a1084
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-19 23:55:49 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 06/Code/AcceptUserInput/AcceptUserInput/js/default.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            args.setPromise(WinJS.UI.processAll());
+        }
+
+        var cityListBox = document.getElementById('cityListBox');
+        var submitButton = document.getElementById('submitButton');
+        var outputSpan = document.getElementById('outputSpan');
+        var rating = document.getElementById('ratingDiv').winControl;
+
+        submitButton.addEventListener('click', function () {
+            outputSpan.innerHTML += cityListBox.value + " : " + rating.userRating + "</br>";
+        });
+    };
+
+    app.start();
+})();
