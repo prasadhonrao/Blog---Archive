dd968e170425c666aa564f3a63007fb5a3069b4b
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-19 23:05:38 +0100

    Modal dialog box functionality implmeneted

--- Windows Store App Development Series/Part 09/Code/Message Dialog Box/MessageDialogBox/js/default.js
+++ Windows Store App Development Series/Part 09/Code/Message Dialog Box/MessageDialogBox/js/default.js
@@ -1,6 +1,4 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
-(function () {
+﻿(function () {
     "use strict";
 
     WinJS.Binding.optimizeBindingReferences = true;
@@ -10,24 +8,27 @@
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
             args.setPromise(WinJS.UI.processAll());
         }
     };
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
+    app.onready = function () {
+        var contentDiv = document.getElementById('contentDiv');
+        var deleteButton = document.getElementById('DeleteButton').winControl;
+
+        deleteButton.addEventListener('click', function () {
+            var dialog = Windows.UI.Popups.MessageDialog("Do you want to delete the content?", "Content Deletion");
+            dialog.commands.append(new Windows.UI.Popups.UICommand("Yes", dialogCommandHandler));
+            dialog.commands.append(new Windows.UI.Popups.UICommand("No", dialogCommandHandler));
+            dialog.defaultCommandIndex = 0;
+            dialog.showAsync();
+        });
+
+        function dialogCommandHandler(command) {
+            if (command.label === 'Yes' && contentDiv.parentElement != null) {
+                contentDiv.parentElement.removeChild(contentDiv);
+            }
+        }
     };
 
     app.start();
