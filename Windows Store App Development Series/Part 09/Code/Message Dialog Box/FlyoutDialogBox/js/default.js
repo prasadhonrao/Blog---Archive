d97fb79e41d5d2db39328542a0d68b76296700bd
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-19 23:12:35 +0100

    Flyout implemented

--- Windows Store App Development Series/Part 09/Code/Message Dialog Box/FlyoutDialogBox/js/default.js
+++ Windows Store App Development Series/Part 09/Code/Message Dialog Box/FlyoutDialogBox/js/default.js
@@ -1,6 +1,4 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
-(function () {
+﻿(function () {
     "use strict";
 
     WinJS.Binding.optimizeBindingReferences = true;
@@ -8,27 +6,36 @@
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
+    var flyout;
+    var deleteCommand;
+    var okCommand;
+
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
             args.setPromise(WinJS.UI.processAll());
         }
     };
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+    app.onready = function (args) {
+        deleteCommand = document.getElementById('DeleteCommand').winControl;
+        flyout = document.getElementById('Flyout');
+        okCommand = document.getElementById('OkButton');
+
+        deleteCommand.addEventListener('click', DisplayFlyout);
+        okCommand.addEventListener('click', DeleteContent);
+    }
+
+    function DisplayFlyout() {
+        flyout.winControl.show(deleteCommand, 'top');
+    }
+
+    function DeleteContent() {
+        var contentDiv = document.getElementById('contentDiv');;
+        if (contentDiv != null && contentDiv.parentElement != null) {
+            contentDiv.parentElement.removeChild(contentDiv);
+        }
+    }
+
 
     app.start();
 })();
