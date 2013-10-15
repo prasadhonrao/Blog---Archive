5c007ae6100400d8b368e09460d387b63288d93c
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-10-15 16:54:39 +0100

    Initial commit

---
+++ Windows Store App Development Series/Part 19/Code/WinJS Promise Chaining/WinJSPromiseChaining/js/default.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var customerList = [];
+    var dataList = [];
+    var result;
+
+    function GetCustomerData(response) {
+        result = response.responseXML;
+    }
+
+    function DisplayCustomerData() {
+        var items = result.querySelectorAll("entry");
+
+        for (var ctr = 0; ctr < items.length; ctr++) {
+            var customer = {};
+            customer.ContactName = items[ctr].querySelector("ContactName").textContent;
+            customer.Country = items[ctr].querySelector("Country").textContent;
+            customerList.push(customer);
+        }
+
+        dataList = new WinJS.Binding.List(customerList);
+        var customerListView = document.getElementById('customerListView').winControl;
+        customerListView.itemDataSource = dataList.dataSource;
+    }
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            var promise = WinJS.xhr({ url: "http://services.odata.org/V3/Northwind/Northwind.svc/Customers" });
+
+            promise.then(GetCustomerData)
+                   .done(DisplayCustomerData);
+
+
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.start();
+})();
