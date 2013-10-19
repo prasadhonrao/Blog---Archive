2367240977a1259504bf6ca894a11352a38a2c34
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-10-19 01:07:16 +0100

    OData service integration completed

--- Windows Store App Development Series/Part 20/Code/WinJS Promise Join/WinJSPromiseJoin/js/default.js
+++ Windows Store App Development Series/Part 20/Code/WinJS Promise Join/WinJSPromiseJoin/js/default.js
@@ -5,6 +5,8 @@
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var customerList = [];
+    var employeeList = [];
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
@@ -16,13 +18,48 @@
 
             WinJS.Promise.join(promises).done(
                 function complete(xhr) {
-                    var x = 0;
+
+                    var customerResult = xhr.customer.responseXML;
+                    var items = customerResult.querySelectorAll("entry");
+
+                    for (var ctr = 0; ctr < items.length; ctr++) {
+                        var customer = {};
+
+                        customer.ContactName = items[ctr].querySelector("ContactName").textContent;
+                        customer.Address = items[ctr].querySelector("Address").textContent;
+                        customer.City = items[ctr].querySelector("City").textContent;
+                        customer.PostalCode = items[ctr].querySelector("PostalCode").textContent;
+                        customer.Country = items[ctr].querySelector("Country").textContent;
+                        customerList.push(customer);
+                    }
+
+                    var customerDataList = new WinJS.Binding.List(customerList);
+                    var customerListView = document.getElementById('CustomerListView').winControl;
+                    customerListView.itemDataSource = customerDataList.dataSource;
+
+                    var employeeResult = xhr.employee.responseXML;
+                    var items = employeeResult.querySelectorAll("entry");
+
+                    for (var ctr = 0; ctr < items.length; ctr++) {
+                        var employee = {};
+                        employee.ContactName = items[ctr].querySelector("FirstName").textContent;
+                        employee.Address = items[ctr].querySelector("Address").textContent;
+                        employee.City = items[ctr].querySelector("City").textContent;
+                        employee.PostalCode = items[ctr].querySelector("PostalCode").textContent;
+                        employee.Country = items[ctr].querySelector("Country").textContent;
+                        employeeList.push(employee);
+                    }
+
+                    var employeeDataList = new WinJS.Binding.List(employeeList);
+                    var employeeListView = document.getElementById('EmployeeListView').winControl;
+                    employeeListView.itemDataSource = employeeDataList.dataSource;
+                    
                 },
                 function error(xhr) {
-                    var x = 0;
+                    // Handle error here...
                 },
                 function progress(xhr) {
-                    var x = 0;
+                    // Handle progress here...
                 }
            );
 
