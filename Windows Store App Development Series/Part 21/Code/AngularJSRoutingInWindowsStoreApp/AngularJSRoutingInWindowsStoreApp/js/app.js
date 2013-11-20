e9d5e69e68bf030ed658599184ddf295e6a0eeb9
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-11-20 16:16:16 +0000

    Routing implemented

---
+++ Windows Store App Development Series/Part 21/Code/AngularJSRoutingInWindowsStoreApp/AngularJSRoutingInWindowsStoreApp/js/app.js
@@ -0,0 +1,31 @@
+﻿var sampleApp = angular.module('sampleApp', []);
+
+sampleApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/AddNewOrder', {
+            templateUrl: 'templates/add_order.html',
+            controller: 'AddOrderController'
+        }).
+        when('/ShowOrders', {
+            templateUrl: 'templates/show_orders.html',
+            controller: 'ShowOrdersController'
+        }).
+        otherwise({
+            redirectTo: '/AddNewOrder'
+        });
+  }]);
+
+
+sampleApp.controller('AddOrderController', function ($scope) {
+
+    $scope.message = 'This is Add new order screen';
+
+});
+
+
+sampleApp.controller('ShowOrdersController', function ($scope) {
+
+    $scope.message = 'This is Show orders screen';
+
+});
