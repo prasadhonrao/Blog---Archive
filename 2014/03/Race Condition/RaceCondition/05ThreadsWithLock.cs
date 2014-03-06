fa855b5cc6ae12636a40a5d445d935cab5dd9cf1
PrasadHonrao <Honrao.Prasad@Gmail.com>
2014-03-06 12:08:16 -0500

    Thread safe code implemented using lock

---
+++ 2014/03/Race Condition/RaceCondition/05ThreadsWithLock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RaceCondition
+{
+    class Program
+    {
+        // Multiple threads accessing shared variable using static method
+
+        static bool isStarPrinted = false;
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            new Thread(PrintStar).Start();
+
+            PrintStar();
+
+            Console.ReadLine();
+        }
+
+        static void PrintStar()
+        {
+            lock (locker) // Thread safe code
+            {
+                if (!isStarPrinted)
+                {
+                    Console.WriteLine(" * ");
+                    isStarPrinted = true;
+                }
+            }
+        }
+    }
+}
