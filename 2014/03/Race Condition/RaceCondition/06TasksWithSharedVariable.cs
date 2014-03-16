9e525a78fffc146a4ef818aeb86b9ed97e4f7232
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-03-15 20:25:37 -0400

    Tasks race with shared variable implemented

---
+++ 2014/03/Race Condition/RaceCondition/06TasksWithSharedVariable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RaceCondition
+{
+    class _06TasksWithSharedVariable
+    {
+        // Simple multi threaded program with shared variable
+
+        private static int counter;
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(PrintPlus);
+            Task.Factory.StartNew(PrintMinus);
+
+            // PrintMinus();
+            Console.ReadLine();
+        }
+
+        static void PrintPlus()
+        {
+            // Console.WriteLine("Thread Id = {0}", Thread.CurrentThread.ManagedThreadId);
+
+            for (counter = 0; counter < 100; counter++)
+            {
+                Console.Write(" + ");
+            }
+        }
+
+        private static void PrintMinus()
+        {
+            // Console.WriteLine("Thread Id = {0}", Thread.CurrentThread.ManagedThreadId);
+
+            for (counter = 0; counter < 100; counter++)
+            {
+                Console.Write(" - ");
+            }
+        }
+    }
+}
