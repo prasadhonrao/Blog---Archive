459ad4c4dec85560d515ba9ba25321424c30e204
PrasadHonrao <Honrao.Prasad@Gmail.com>
2014-04-11 13:54:40 -0400

    Initial commit

---
+++ 2014/04/Top 10 JavaScript Traps For C# Developer/Top10Traps/ConsoleApp/SwitchStatement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class SwitchStatement
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age");
+            var age = Console.ReadLine();
+
+            switch (age)
+            {
+                case "10":
+                    Console.WriteLine("Take Milk");
+                    break;
+                case "30":
+                    Console.WriteLine("Take Beer");
+                    break;
+                case "50":
+                    Console.WriteLine("Take Medicine");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
