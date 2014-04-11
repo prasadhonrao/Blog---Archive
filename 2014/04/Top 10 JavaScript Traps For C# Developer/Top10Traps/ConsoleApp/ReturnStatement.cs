11a49e1bccf4b2b10d2f686f1da3813167f6a0e4
PrasadHonrao <Honrao.Prasad@Gmail.com>
2014-04-11 14:36:33 -0400

    Return statement example added

---
+++ 2014/04/Top 10 JavaScript Traps For C# Developer/Top10Traps/ConsoleApp/ReturnStatement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class ReturnStatement
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(10, 20));
+
+            Console.WriteLine(DefaultEmployee().Name);
+            Console.ReadLine();
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static Employee DefaultEmployee()
+        {
+            return new Employee
+                    {
+                        Id = 1,
+                        Name = "Default"
+                    };
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
