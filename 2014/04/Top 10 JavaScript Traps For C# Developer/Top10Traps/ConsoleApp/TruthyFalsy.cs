81dde3a9c256f45792ffd62889b77941bfb57772
PrasadHonrao <Honrao.Prasad@Gmail.com>
2014-04-11 15:07:49 -0400

    Truthy and Falsy examples added

---
+++ 2014/04/Top 10 JavaScript Traps For C# Developer/Top10Traps/ConsoleApp/TruthyFalsy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class TruthyFalsy
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = 10;
+            var secondNumber = "10";
+
+            // Below statements will result into compile time error
+            //if (firstNumber == secondNumber) { }
+            //while (1) {}
+            //while (0) {}
+
+            // Converting number to string and then typecast to bool
+            var equalNumber = ((bool)(firstNumber.ToString() == secondNumber));
+            Console.WriteLine(equalNumber);
+            Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("In True");
+            }
+
+            
+        }
+    }
+}
