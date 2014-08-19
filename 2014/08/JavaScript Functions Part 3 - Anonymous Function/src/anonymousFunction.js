743a79ab79345c30acbee32ac301191287186f72
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-19 15:40:48 -0400

    Different examples of anonymous function declaration added

--- 2014/08/JavaScript Functions Part 3 - Anonymous Function/src/anonymousFunction.js
+++ 2014/08/JavaScript Functions Part 3 - Anonymous Function/src/anonymousFunction.js
@@ -1,13 +1,44 @@
-
+//Anonymous Functions
 var log = function(message){
 	console.log(message);
 }
-log("This is an anonymous function");
+log("Jack & Jill went up the hill...");
 
-//Anonymous Functions In Object Literal
-var person = {
-    sayHello: function(){
-        console.log("hello");
-    }
-};
-person.sayHello();
+// //Anonymous Functions In Object Literal
+// var person = {
+//     firstName : "Prasad",
+//     lastName : "Honrao",
+//     fullName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+// console.log(person.fullName());
+
+// // As function object
+// setTimeout(function() {
+//     console.log('hello');
+// }, 1000);
+
+
+// // In Node.js
+
+// var http = require("http");
+// var server = http.createServer(function(request, response) {
+//     response.write("Hello World");
+//     response.end();
+
+// });
+// server.listen(1234);
+
+// // Callback function
+// var contest = {
+//                 ans: "Red",
+//                 askQuestion: function (question, answered) {
+//                     console.log(question);
+//                     answered(this.ans);
+//                }
+//             }
+
+// contest.askQuestion("What is your favorite color?" , function (answer) {
+//     console.log("Answer is " + answer);
+// });
