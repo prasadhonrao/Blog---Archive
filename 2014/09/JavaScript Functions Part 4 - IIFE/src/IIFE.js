30d79b56f7dc2bd304d946f2a3a5cf0ff371336b
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-10 12:42:18 -0400

    IIFE documentation and code completed

--- 2014/09/JavaScript Functions Part 4 - IIFE/src/IIFE.js
+++ 2014/09/JavaScript Functions Part 4 - IIFE/src/IIFE.js
@@ -1,28 +1,53 @@
-(function() {
-	var payroll = function() {
-	    var audit = function(info) {
-	        console.log("Auditing information " + info)
-	    }
+// (function() {
+// 	var payroll = function() {
+// 	    var audit = function(info) {
+// 	        console.log("Auditing information " + info)
+// 	    }
 
-	    var makePayment = function () {
-	        console.log("In MakePayment function");
-	        audit("success");
-	    };
+// 	    var makePayment = function () {
+// 	        console.log("In MakePayment function");
+// 	        audit("success");
+// 	    };
 
-	    var calculcateOverTime = function () {
-	        console.log("In CalculateOverTime function");
-	        audit("success");
-	    };
+// 	    var calculcateOverTime = function () {
+// 	        console.log("In CalculateOverTime function");
+// 	        audit("success");
+// 	    };
 
-	    return {
-	        MakePayment : makePayment,
-	        CalculateOverTime : calculcateOverTime
-	    }
-	};
+// 	    return {
+// 	        MakePayment : makePayment,
+// 	        CalculateOverTime : calculcateOverTime
+// 	    }
+// 	};
 
-	var payrollAdmin = payroll();
-	payrollAdmin.MakePayment();
-	payrollAdmin.CalculateOverTime();
+// 	var payrollAdmin = payroll();
+// 	payrollAdmin.MakePayment();
+// 	payrollAdmin.CalculateOverTime();
+// }());
+
+
+
+var payroll = (function() {
+    var audit = function(info) {
+        console.log("Auditing information " + info)
+    }
+
+    var makePayment = function () {
+        console.log("In MakePayment function");
+        audit("success");
+    };
+
+    var calculcateOverTime = function () {
+        console.log("In CalculateOverTime function");
+        audit("success");
+    };
+
+    return {
+        MakePayment : makePayment,
+        CalculateOverTime : calculcateOverTime
+    }
 }());
 
+payroll.MakePayment();
+payroll.CalculateOverTime();
 
