4a48da7e1bfc89f6036c1d6c5f08730b362884c8
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-12-03 19:15:48 -0500

    Initial commit

---
+++ 2014/12/C# 6.0 Language Features - Static Using Statements/src/Static Using Statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Console;
+
+namespace LanguageFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor textColor = ForegroundColor;
+            try
+            {
+                ForegroundColor = ConsoleColor.Red;
+                Write("Enter your name ");
+
+                ForegroundColor = ConsoleColor.Blue;
+                string name = ReadLine();
+
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Hello {0}, welcome to C# 6.0!", name);
+
+                ReadLine();
+            }
+            finally
+            {
+                ForegroundColor = textColor;
+            }
+        }
+    }
+}
