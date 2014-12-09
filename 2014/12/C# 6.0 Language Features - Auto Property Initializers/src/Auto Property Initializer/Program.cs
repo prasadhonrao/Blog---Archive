00fb643232d0e69e7bdb49697c4d5930c8f67496
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-12-08 20:33:15 -0500

    Initial commit
    Code file and article content added

---
+++ 2014/12/C# 6.0 Language Features - Auto Property Initializers/src/Auto Property Initializer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Console;
+
+namespace Auto_Property_Initializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oldCustomer = new Customer("Mumbai");
+            var newCustomer = new CustomerV2();
+            WriteLine(oldCustomer.City);
+            WriteLine(newCustomer.City);
+            ReadLine();
+        }
+    }
+
+    public class Customer
+    {
+        public string City { get; private set; }
+
+        public Customer(string city)
+        {
+            this.City = city;
+        }
+    }
+
+    public class CustomerV2
+    {
+        public string City { get; } = "London";
+    }
+}
